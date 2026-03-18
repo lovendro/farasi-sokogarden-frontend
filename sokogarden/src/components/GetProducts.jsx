a23ac0b33211fce6637df24001c7b8b89c9cb68e
lovendro <lovendro.p@gmail.com>
2026-03-18 13:38:15 +0300

    I added the mpesa component that allows payment of goods by the user

--- sokogarden/src/components/GetProducts.jsx
+++ sokogarden/src/components/GetProducts.jsx
@@ -1,5 +1,6 @@
 import React, { useState,useEffect } from 'react'
 import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
 
 const GetProducts = () => {
 
@@ -7,6 +8,7 @@ const GetProducts = () => {
   const[error,setError]=useState("")
   const[products,setProducts]=useState([])
   const image_url="http://lovendro.alwaysdata.net/static/images/"
+  const navigate=useNavigate()
 
 
   const fetchproducts=async()=>{
@@ -42,13 +44,13 @@ const GetProducts = () => {
 
 
         <div className='col-md-3 justify-content-centre'>
-          <div className='card shadow m-3'>
+          <div className='card shadow m-4'>
             <img src={image_url + product.product_photo} alt="shoe" className='product_img'/>
             <div className='card-body'>
               <h5 className='text-success'>{product.product_name}</h5>
-              <p className='text-secondary'>{product.product_description}</p>
-              <p className='text-warning'>{product.product_cost}</p>
-              <input type="button" className='btn btn-secondary w-100' value='Make payment'/>
+              <p className='text-dark'>{product.product_description}</p>
+              <p className='text-secondary'>{product.product_cost}</p>
+              <input type="button" className='btn btn-secondary w-100' value='Purchase now' onClick={()=>navigate("/mpesa",{state:{product}})}/>
 
             </div>
 
--- sokogarden/src/components/MpesaComponent.jsx
+++ sokogarden/src/components/MpesaComponent.jsx
@@ -1,9 +1,67 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useLocation } from 'react-router-dom'
+import axios from 'axios'
 
 const MpesaComponent = () => {
+  const[loading,setLoading]=useState("")
+  const[success,setSuccess]=useState("")
+  const[error,setError]=useState("")
+  
+
+  const[phone,setPhone]=useState("")
+
+  const{product}=useLocation().state || {}
+  // A function to submit data to our api
+
+  const submit=async(e)=>{
+    e.preventDefault()
+    setLoading("Please wait as we process your payment")
+
+    try {
+      const data=new FormData()
+      data.append("phone",phone)
+      data.append("amount",product.product_cost)
+
+
+      const response=await axios.post("http://lovendro.alwaysdata.net/api/mpesa_payment",data)
+      setLoading("")
+      setSuccess(response.message)
+      
+    } catch (error) {
+      setLoading("")
+      
+        setError(error.message)
+        
+      
+      
+    }
+
+
+
+
+  }
+
+  
   return (
-    <div>
-        <h1>Welcome to Mpesa component</h1>
+    <div className='row justify-content-center'>
+        <h1>Mpesa payment-Lipa na Mpesa</h1>
+        <p className='text-success'>{product.product_name}</p>
+        <p className='text-dark'>{product.product_description}</p>
+        <p className='text-secondary'>{product.product_cost}</p>
+
+        <p className='text-warning'>{loading}</p>
+        <p className='text-success'>{success}</p>
+        <p className='text-danger'>{error}</p>
+
+        <div className='col-md-6'>
+          <form action="" onSubmit={submit}>
+            <input type="tel" className='form-control' placeholder='Enter your phone starting with 254' value={phone} required onChange={(e)=>setPhone(e.target.value)}/>
+            <br />
+            <input type="submit" className='btn btn-secondary  w-100' value="Make payment" required />
+            
+          </form>
+
+        </div>
     </div>
   )
 }
